b8be2a73557cd8cd1bdd735de8df142531e3ea3d
Raman Madhukar <ramanmadhukar6@gmail.com>
2022-11-08 19:43:12 +0530

    form design sundar hai

--- src/Pages/Form.js
+++ src/Pages/Form.js
@@ -6,6 +6,8 @@ import { useNavigate } from "react-router-dom";
 
 const Form = () => {
 
+
+
     const initialValue = {
         name: '',
         age: '',
@@ -21,6 +23,7 @@ const Form = () => {
         lat: '',
         lng: ''
     })
+    const [loaction, setloaction] = useState(true)
 
     const { name, age, disease } = patient;
 
@@ -51,29 +54,62 @@ const Form = () => {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition((e) => {
                 setPosition({ lat: e.coords.latitude, lng: e.coords.longitude })
+                setloaction(true);
             });
         } else {
             console.log("Geo Location not supported by browser");
+            setloaction(false);
         }
-    }, [])
+    }, []);
+
+    // const fetchLoaction = (event) => {
+    //     event.preventDefault();
+    //     navigator.geolocation.getCurrentPosition((e) => {
+    //         setPosition({ lat: e.coords.latitude, lng: e.coords.longitude })
+    //         // setloaction(true);
+    //     });
+
+    // }
+
+
+
 
 
     return (
-        <form className='form' >
-            <div className='fromDiv'>
-                <label className="form-label mb-3" htmlFor="name">Name</label>
-                <input className="form-control mb-3" name='name' type="text" placeholder='Name' onChange={handleChange('name')} value={name} />
-                <label className="form-label mb-3" htmlFor="age">Age</label>
-                <input className="form-control mb-3" name='age' type="number" placeholder='Age' onChange={handleChange('age')} value={age} />
-                <label className="form-label mb-3" htmlFor="disease">Disease</label>
-                <select className="form-select mb-3" name="disease" onChange={handleChange('disease')} value={disease} >
-                    <option value="dengue">Dengue</option>
-                    <option value="maleria">Maleria</option>
-                </select>
-                <button className='btn btn-primary' onClick={clickSubmit}>Submit</button>
-            </div>
-        </form>
+
+        <>
+            {
+                loaction ?
+
+                    (
+                        <form className='form' >
+                            <div className='fromDiv'>
+                                <p className='mb-3'><h2  >Fill health status </h2>(location access is mandatory)</p>
+                                <label className="form-label mb-3" htmlFor="name">Name</label>
+                                <input className="form-control mb-3" name='name' type="text" placeholder='Name' onChange={handleChange('name')} value={name} />
+                                <label className="form-label mb-3" htmlFor="age">Age</label>
+                                <input className="form-control mb-3" name='age' type="number" placeholder='Age' onChange={handleChange('age')} value={age} />
+                                <label className="form-label mb-3" htmlFor="disease">Disease</label>
+                                <select required className="form-select mb-3" name="disease" onChange={handleChange('disease')} value={disease} >
+                                    <option value="" disabled>Select</option>
+                                    <option value="dengue">Dengue</option>
+                                    <option value="nfluenza">Influenza</option>
+                                    <option value="maleria">Malaria</option>
+                                </select>
+                                {/* <button className='btn btn-primary' onClick={fetchLoaction}>loaction</button> */}
+                                <button className='btn btn-primary' onClick={clickSubmit}>Submit</button>
+                            </div>
+                        </form>
+                    )
+                    :
+                    (
+                        <p>Please allow loaction</p>
+                    )
+            }
+        </>
+
     )
+
 }
 
 export default Form
