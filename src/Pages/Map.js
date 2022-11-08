84ed10e4a21d9907cc757e0fad092b3d642dd8ed
Raman Madhukar <ramanmadhukar6@gmail.com>
2022-11-08 22:51:26 +0530

    info div

--- src/Pages/Map.js
+++ src/Pages/Map.js
@@ -2,10 +2,17 @@ import React, { useEffect, useState } from 'react'
 import { Circle, MapContainer, TileLayer } from "react-leaflet";
 import LocationMarker from "../Components/LocationMarker";
 import axios from 'axios';
+import './Map.css'
 
 const Map = () => {
 
     const [points, setPoints] = useState([]);
+    // const [dengue, setDengue] = useState();
+    // const [malaria, setmalaria] = useState();
+    // const [influenza, setInfluenza] = useState();
+    var dengue = 0;
+    var malaria = 0;
+    var influenza = 0;
 
     const fillBlueOptions = { fillColor: 'red' }
 
@@ -23,25 +30,47 @@ const Map = () => {
             });
     }, [])
 
+    points.map((e) => {
+        if (e.disease === 'dengue') {
+            dengue++;
+        }
 
-    console.log(points);
+        else if (e.disease === 'malaria') {
+            malaria++;
+        }
+
+        else if (e.disease === 'influenza') {
+            influenza++;
+        }
+        return 0;
+    })
+
+
 
     return (
         <>
-            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
+            <div className="infro">
+                <p className='mb-2'>You are at this loaction</p>
+                <p className='mb-2'>Dengue: {dengue}</p>
+                <p className='mb-2'>Malaria: {malaria}</p>
+                <p className='mb-2'>Influenza: {influenza}</p>
+            </div>
+            <div className="mapDiv">
+                <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
 
-                <LocationMarker />
+                    <LocationMarker />
 
 
-                {points.map(i =>
-                    <Circle center={[i.lat, i.lng]} pathOptions={fillBlueOptions} radius={20} />
-                )}
+                    {points.map(i =>
+                        <Circle center={[i.lat, i.lng]} pathOptions={fillBlueOptions} radius={20} />
+                    )}
 
-            </MapContainer>
+                </MapContainer>
+            </div>
 
         </>
     );
